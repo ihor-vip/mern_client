2bd988bcf1a500bc24a3d52410fd606ea2160328
ihor-vip <vip20140902@gmail.com>
2023-04-20 13:49:52 +0300

    Register component

--- src/helper/validate.js
+++ src/helper/validate.js
@@ -22,6 +22,26 @@ export async function resetPasswordValidation(values){
     return errors;
 }
 
+function emailVerify(error ={}, values){
+    if(!values.email){
+        error.email = toast.error("Email Required...!");
+    }else if(values.email.includes(" ")){
+        error.email = toast.error("Wrong Email...!")
+    }else if(!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        error.email = toast.error("Invalid email address...!")
+    }
+
+    return error;
+}
+
+export async function registerValidation(values){
+    const errors = usernameVerify({}, values);
+    passwordVerify(errors, values);
+    emailVerify(errors, values);
+
+    return errors;
+}
+
 function passwordVerify(errors = {}, values){
     /* eslint-disable no-useless-escape */
     const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
